91185a784a266dea477563895ec7139d6fbd7958
theluanz <luanzuffo.lz@gmail.com>
2022-03-02 15:22:35 -0300

    search course

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -1,3 +1,4 @@
+import { CourseService } from './../course.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../courses';
 
@@ -7,31 +8,26 @@ import { Course } from '../courses';
   styleUrls: ['./course-list.component.css'],
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
-  constructor() {}
+  _courses: Course[] = [];
+  _filterBy: string = '';
+  filteredCourse: Course[] = [];
+
+  constructor(private couserService: CourseService) {}
 
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 22.5,
-        code: 'THZ-313',
-        duration: 120,
-        rating: 4.5,
-        relaseDate: 'February, 25, 2022',
-      },
-      {
-        id: 2,
-        name: 'Angular CLI ',
-        imageUrl: '/assets/images/cli.png',
-        price: 32.5,
-        code: 'THZ-237',
-        duration: 50,
-        rating: 3,
-        relaseDate: 'February, 20, 2022',
-      },
-    ];
+    this._courses = this.couserService.retriveAll();
+    this.filteredCourse = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredCourse = this._courses.filter(
+      (cource: Course) =>
+        cource.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1
+    );
+  }
+  get filter() {
+    return this._filterBy;
   }
 }
--- src/app/courses/courses.ts
+++ src/app/courses/courses.ts
@@ -1,10 +1,11 @@
 export interface Course {
   id: number;
   name: string;
+  description: string;
   imageUrl: string;
   price: number;
   code: string;
   duration: number;
   rating: number;
-  relaseDate: string;
+  releaseDate: string;
 }
